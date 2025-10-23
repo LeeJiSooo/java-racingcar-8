8b0cf6b3fdc46a911b55a604bb3a82f4ad57b562
이지수 <0801ljs@naver.com>
2025-10-23 15:54:15 +0900

    feat: 자동차 이름 및 시도 횟수 입력 기능 구현

--- src/main/java/racingcar/controller/GameController.java
+++ src/main/java/racingcar/controller/GameController.java
@@ -1,7 +1,17 @@
 package racingcar.controller;
 
+import racingcar.view.InputView;
+
 public class GameController {
+    private final InputView inputView;
+    public GameController() {
+        this.inputView = new InputView();
+    }
     public void run() {
-        System.out.println("게임 컨트롤러 실행");
+        String carNamesInput = inputView.readCarNames();
+        String tryCountInput = inputView.readTryCount();
+
+        System.out.println("입력된 이름: " + carNamesInput);
+        System.out.println("입력된 횟수: " + tryCountInput);
     }
 }
