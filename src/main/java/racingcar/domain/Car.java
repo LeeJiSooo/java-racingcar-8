6f58dc651c0b47d62b4d9347130a35e303fb38c6
이지수 <0801ljs@naver.com>
2025-10-23 15:58:38 +0900

    feat(domain): Car 객체 구현

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,4 +1,25 @@
 package racingcar.domain;
 
 public class Car {
+    private final String name;
+    private int position;
+
+    public Car(String name) {
+        // 이름은 Controller에서 검증 후 넘어온다고 가정
+        this.name = name.trim(); // 혹시 모를 공백 제거
+        this.position = 0;
+    }
+    public void move(int number) {
+        if (number >= 4) {
+            this.position++;
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
 }
