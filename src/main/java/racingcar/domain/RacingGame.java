e833ef180f5d36ae07e9697296455344a56d2d20
이지수 <0801ljs@naver.com>
2025-10-23 16:01:18 +0900

    feat(domain): RacingGame 객체 및 1라운드 실행 기능 구현

--- src/main/java/racingcar/domain/RacingGame.java
+++ src/main/java/racingcar/domain/RacingGame.java
@@ -1,4 +1,50 @@
 package racingcar.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class RacingGame {
+    private final List<Car> cars;
+
+    public RacingGame(List<Car> cars) {
+        this.cars = cars;
+    }
+    // 한 라운드를 진행합니다.
+    public void playRound() {
+        for (Car car : cars) {
+            int randomNumber = Randoms.pickNumberInRange(0, 9);
+            car.move(randomNumber);
+        }
+    }
+
+    // 현재 자동차들의 상태를 반환
+    public List<Car> getCars() {
+        return cars;
+    }
+
+    // 우승자 판별
+    public List<Car> getWinners() {
+        int maxPosition = findMaxPosition();
+        List<Car> winners = new ArrayList<>();
+
+        for (Car car : cars) {
+            if (car.getPosition() == maxPosition) {
+                winners.add(car);
+            }
+        }
+        return winners;
+    }
+
+    // 우승자를 찾기 위해 최대 위치를 찾는 내부 메서드
+    private int findMaxPosition() {
+        int maxPosition = 0;
+        for (Car car : cars) {
+            if (car.getPosition() > maxPosition) {
+                maxPosition = car.getPosition();
+            }
+        }
+        return maxPosition;
+    }
 }
