b30217753f7c4d35dc1c8d6a0ae39cdf9015fad4
이지수 <0801ljs@naver.com>
2025-10-27 18:21:21 +0900

    fix(Application): try catch fix

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -5,11 +5,7 @@ import racingcar.controller.GameController;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        try {
-            GameController gameController = new GameController();
-            gameController.run();
-        } catch (IllegalArgumentException e) {
-            System.out.println("에러메시지" + e.getMessage());
-        }
+        GameController gameController = new GameController();
+        gameController.run();
     }
 }
